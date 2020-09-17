17bd56641b3179e797700dd5cf3e5489cc8b2a79
JohnLea10197 <ethan@localhost.localdomain>
2020-09-17 18:20:19 -0400

    Header and footer templates, header template has customizable sub-header text. Cleaned up the code.

---
+++ includes/js/footer.js
@@ -0,0 +1,1 @@
+document.write('\<footer> <a href="mailto:johnlea10197@gmail.com">Email</a> \<br>\ <a href="https://www.github.com/JohnLea10197/" target="_blank">Github</a> \<br>\ </footer>')
---
+++ includes/js/header.js
@@ -0,0 +1,3 @@
+var pageType = " "; 
+pageType = document.getElementById("pageType").value;
+document.write('\ <h1>\ <div class="title">\ John Lea\ </div>\ <br>\ <div class="title2">\ ',  pageType, ' \</div>\ </h1>\ ');
